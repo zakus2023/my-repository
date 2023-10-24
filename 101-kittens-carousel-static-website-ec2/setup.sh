c8a860e394d260e722ed55cfa1f3fb73ecd084a0
abdul <idbsch2012@gmail.com>
2023-10-23 21:36:23 -0400

    added my name

---
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -0,0 +1,7 @@
+!# /bin/bash
+
+FOLDER="https://raw.githubusercontent.com/zakus2023/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
