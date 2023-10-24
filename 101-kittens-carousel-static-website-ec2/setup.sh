5b768faf9a1eeffc4df0a65f3fe85bb9f0ac2f61
abdul <idbsch2012@gmail.com>
2023-10-23 22:13:58 -0400

    Final work

--- 101-kittens-carousel-static-website-ec2/setup.sh
+++ 101-kittens-carousel-static-website-ec2/setup.sh
@@ -1,7 +1,14 @@
-!# /bin/bash
+#!/bin/bash -x
+
+yum update -y
+yum install httpd -y
+cd /var/www/html
 
 FOLDER="https://raw.githubusercontent.com/zakus2023/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
 wget ${FOLDER}/index.html
 wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
 wget ${FOLDER}/cat2.jpg
+
+systemctl start httpd
+systemctl enable httpd
